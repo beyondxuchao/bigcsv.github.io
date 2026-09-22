30771dd21e609e58fe90be743766bd92fedb202a
shuxge <shuxge@126.com>
2026-09-22 17:44:39 +0800

    Remove donation banner from all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Poppins } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
-import { DonationBanner } from '@/components/DonationBanner';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -64,7 +63,6 @@ export default function RootLayout({
       >
 
         <Header />
-        <DonationBanner />
         <main className="flex-1">
           {children}
         </main>
